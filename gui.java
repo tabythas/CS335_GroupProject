a6adaa459df3349cd65857f8143a84f6d7cfbfe2
tabythas <tabythasmith@simmons.edu>
2022-09-28 21:46:10 -0400

    Modified structure for panels to appear/disappear

--- gui.java
+++ gui.java
@@ -1,47 +1,86 @@
 import java.awt.*;
+import java.io.File;
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 
 public class gui implements ActionListener {
-
-    private JLabel label;
-    private JFrame frame;
-    private JPanel panel;
+    private JFrame mainFrame;
+    private JPanel mainPanel;
+    private JLabel mainLabel;
+    private JPanel firstQuestion;
     
     public gui() throws IOException{
-        frame = new JFrame();
-        JButton button = new JButton("Start Quiz");
-        button.addActionListener(this);
+        // create main frame
+        mainFrame = new JFrame();
+        
+        // button for starting the quiz
+        JButton mainButton = new JButton("Start Quiz");
+        // need to connect the listener to the reciever 
+        mainButton.addActionListener(this);
 
-        label = new JLabel("Test Your Art Knowledge (or lack thereof)!");
+        // label to show the user what they are using
+        mainLabel = new JLabel("Test Your Art Knowledge (or lack thereof)!");
 
-        panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(200, 200, 200, 200));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(label);
-        panel.add(button);
-
-
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Art Quiz");
-        frame.pack();
-        frame.setVisible(true);
+        // each "page" will be a panel
+        mainPanel = new JPanel();
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(300, 300, 300, 300));
+        mainPanel.setLayout(new GridLayout(0, 1));
+        // need to add components into the panel 
+        mainPanel.add(mainLabel);
+        mainPanel.add(mainButton);
+        
+        // add main panel to main frame
+        mainFrame.add(mainPanel, BorderLayout.CENTER);
+        // application will close if exit is clicked
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // setting frame title
+        mainFrame.setTitle("Art Quiz");
+        mainFrame.pack();
+        mainFrame.setVisible(true);
 
     }
+    // main method to call and build application
     public static void main(String[] args) throws IOException{
         new gui();
     }
 
+    // method to connect the first button to an action when clicked
     public void actionPerformed(ActionEvent event){
-        frame.dispose();
+        // when mainButton is clicked, the mainPanel will be invisible
+        mainPanel.setVisible(false);
+       
         try {
-            new additional_frame();
+            // build next panel
+            firstQuestion = new JPanel();
+            firstQuestion.setBorder(BorderFactory.createEmptyBorder(200, 200, 200, 200));
+
+            JButton firstQuestionButton = new JButton("Next Question");
+            firstQuestion.add(firstQuestionButton);
+            
+            // for now, gathering image data from file. Will need to figure out import
+            File file = new File("C:\\Users\\tabyt\\OneDrive\\Desktop\\CS335_SoftwareEngineering\\CS335_GroupProject\\impression_sunrise.png");
+            BufferedImage bufferedImage = ImageIO.read(file);
+            ImageIcon image = new ImageIcon(bufferedImage);
+            // need label to hold image
+            JLabel firstImage = new JLabel();
+            // add image to panel by setting icon
+            firstImage.setIcon(image);
+            firstQuestion.add(firstImage);
+            // add firstQuestion panel to the mainFrame for it to be visible
+            mainFrame.add(firstQuestion, BorderLayout.CENTER);
+
+            // when first question is clicked, panel is set to visible=false
+            firstQuestionButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent ae) {
+                    firstQuestion.setVisible(false);
+                }
+            });
         }
         catch (IOException ioe){
             ioe.printStackTrace(System.err);
         }
-       
     }
 }    
